82c19d361dfd8ff058ca637d1776d752f03c833f
Zeruost <josmithg@gmail.com>
2014-06-03 22:40:03 -0400

    Cambios v 1.1
    Uní la pantalla de inicio a la del juego, el juego fluye de inicio a la
    del juego sin problema. Corregí el problema que obligaba al programa a
    recrear el marco con cada acción. Cambie el puntero para que muestre
    hacia donde apunta el personaje.
    Introduje coordenadas en pantalla del juego para que sea mas fácil de
    visualizar para nosotros (tiene un error que no pude arreglar eso si, si
    en numero es menor a 10, lo muestra x10. Ejemplo, si estas en las
    coordenadas (9,53), muestra (90,53).

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,17 @@
 #include <curses.h>
+#include <string>
+using namespace std;
+
+void disparo(int y, int x){     //Experimental, no funciona...
+
+    //for (int i=y; y<0;y--)
+    {
+    mvprintw(y-1, x, "|");
+    refresh();
+    }
+
+
+}
 
 void marco(){
 
@@ -18,37 +31,90 @@ void marco(){
 
 int main()
 {
-    int x = 35;
+
+    // Pantalla de Inicio ---------------------------------------------------------------
+
+    bool eleccion = true;
+    int x = 31;
     int y = 13;
     int ch;
     initscr();
     curs_set(0);
     keypad(stdscr, TRUE);
     noecho();
+    marco();
+
+    while (eleccion)
+    {
+        mvprintw(8,33,"Shoot Many X");
+        mvprintw(13,33,"Iniciar Juego");
+        mvprintw(14,33,"Controles");
+        mvprintw(15,33,"Creditos");
+        mvprintw(16,33,"Salir");
+        marco();
+        mvprintw(y, x, ">");
+        ch = getch();
+        if (ch == KEY_DOWN && y<16)
+        {
+            mvprintw(y, x, " ");
+            y++;
+        }
+        if (ch == KEY_UP && y>13)
+        {
+            mvprintw(y, x, " ");
+            y--;
+        }
+        if (ch == ' ' && y == 13)
+        {
+            eleccion = false;
+        }
+        refresh();
+    }
+        clear();
+
+    // Pantalla de juego ----------------------------------------------------------------
+
+    x = 35;
+    y = 13;
+    initscr();
+    curs_set(1);                //Recordar dejarlo en 0, lo puse en 1 para saber donde esta por el momento
+    keypad(stdscr, TRUE);
+    noecho();
+    mvprintw(y, x, "^");
+    marco();
     while (1)
     {
-        marco();
-        mvprintw(y, x, "X");
+        mvprintw(2,34,"Y: %d",y);
+        mvprintw(2,40,"X: %d",x);
         ch = getch();
         if (ch == KEY_DOWN && y<20)
         {
+            mvprintw(y, x, " ");
             y++;
-            clear();
+            mvprintw(y, x, "v");
         }
         if (ch == KEY_UP && y>4)
         {
+            mvprintw(y, x, " ");
             y--;
-            clear();
+            mvprintw(y, x, "^");
         }
         if (ch == KEY_LEFT && x>4)
         {
+            mvprintw(y, x, " ");
             x--;
-            clear();
+            mvprintw(y, x, "<");
         }
         if (ch == KEY_RIGHT && x<75)
         {
+            mvprintw(y, x, " ");
             x++;
-            clear();
+            mvprintw(y, x, ">");
+        }
+       if (ch == ' ')
+        {
+           move(y,x);
+           printw("Insertar Disparo Aqui");
         }
         refresh();
     }
